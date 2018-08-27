11230648811768f99a2740e8d3144631ef4664f3
Velázquez Cruz Rodrigo Fernando <rodrigo_velazquez99@ciencias.unam.mx>
2018-08-26 22:00:53 -0500

    Archivo inicial del cliente

--- src/Cliente.go
+++ src/Cliente.go
@@ -1,3 +1,20 @@
 package main
 
-//import "net"
+import (
+	"fmt"
+	"io/ioutil"
+	"net"
+)
+
+func nuevoUsuario() {
+	direccion, errl := net.ResolveTCPAddr("tcp", "192.168.0.16:8080")
+	if errl != nil {
+		errl.Error()
+	}
+	user, err := net.DialTCP("tcp", nil, direccion)
+	if err != nil {
+		err.Error()
+	}
+	mensaje, _ := ioutil.ReadAll(user)
+	fmt.Println(string(mensaje))
+}
