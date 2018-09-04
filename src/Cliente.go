fe6b8e93848e758894e415d727acbf3ce7c6b8f6
Velázquez Cruz Rodrigo Fernando <rodrigo_velazquez99@ciencias.unam.mx>
2018-09-04 13:56:06 -0500

    Mejoras a la comunicacion de varios clientes

--- src/Cliente.go
+++ src/Cliente.go
@@ -8,27 +8,30 @@ import (
 )
 
 func main() {
+
 	if len(os.Args) != 3 {
 		fmt.Println("No agregaste correctamente los datos de la direccion y puerto")
 		os.Exit(1)
 	}
-	direccionIP := os.Args[1]
-	puerto := os.Args[2]
+
 	var nombre string
 	fmt.Println("Introduce el nombre de usuario")
 	fmt.Scanln(&nombre)
-	fmt.Println("Bienvenido" + nombre)
-	//agregaUsuario(nombre)
-	conexion, err := net.Dial("tcp", direccionIP+":"+puerto)
+	fmt.Println("Bienvenido " + nombre)
+	conexion, err := net.Dial("tcp", os.Args[1] + ":" + os.Args[2])
+
 	if err != nil {
 		err.Error()
 	}
+	//Servidor.agregaUsuario(nombre, conexion)
 	for {
+
 		informacionDevuelta := bufio.NewReader(os.Stdin)
 		fmt.Print(nombre + ">")
 		lector, _ := informacionDevuelta.ReadString('\n')
 		fmt.Fprintf(conexion, lector+"\n")
 		mensaje, _ := bufio.NewReader(conexion).ReadString('\n')
 		fmt.Print(string(mensaje))
+
 	}
 }
--- src/Servidor.go
+++ src/Servidor.go
@@ -9,6 +9,7 @@ import (
 
 type Usuario struct {
 	nombre string
+	conexion net.Conn
 }
 
 var listaUsuarios []Usuario
@@ -16,28 +17,35 @@ var listaUsuarios []Usuario
 /* Inicia un servidor */
 func main() {
 
-	fmt.Println("Iniciando el servidor ..... ")
+	if len(os.Args) != 3 {
+		fmt.Println("No agregaste correctamente los datos de la direccion y puerto")
+		os.Exit(1)
+	}
 
-	servidor, conexion1 := net.Listen("tcp", "localhost:8080")
+	fmt.Println("  Iniciando el servidor ..... ")
+
+	servidor, conexion1 := net.Listen("tcp", os.Args[1] + ":" + os.Args[2])
 
 	listaUsuarios = make([]Usuario, 1)
 
 	revisaError(conexion1)
 
-	conexion, disponible := servidor.Accept()
+	//conexion, disponible := servidor.Accept()
 
 	for {
+		conexion, disponible := servidor.Accept()
 		if disponible != nil {
 			fmt.Println("Esperando conexion")
 			continue
 		}
-		manejaCliente(conexion)
+		 manejaCliente(conexion)
 	}
 }
 
-func agregaUsuario(nombre string) {
+func agregaUsuario(nombre string, conexion net.Conn) {
 	var nuevoUsuario Usuario
 	nuevoUsuario.nombre = nombre
+	nuevoUsuario.conexion = conexion
 	listaUsuarios = append(listaUsuarios, nuevoUsuario)
 }
 
@@ -48,11 +56,20 @@ func revisaError(err error) {
 }
 
 func manejaCliente(conexion net.Conn) {
-	mensaje, _ := bufio.NewReader(conexion).ReadString('\n')
-	fmt.Println(string(mensaje))
-	conexion.Write([]byte(mensaje))
-	if mensaje == "/close" {
-		os.Exit(1)
+	var usuarioActual string
+	for _, u  := range listaUsuarios {
+		if u.conexion == conexion{
+		usuarioActual = u.nombre
+		}
 	}
-
+	for {
+		mensaje, _ := bufio.NewReader(conexion).ReadString('\n')
+		fmt.Println(usuarioActual + ": " + string(mensaje))
+		conexion.Write([]byte(mensaje))
+		if mensaje == "/close" {
+			conexion.Close()
+			os.Exit(1)
+			break
+		}
+	}
 }
