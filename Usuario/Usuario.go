a6f03c3fbe577c39854e4eec55fef78912ba973c
Rodrigo Fernando Velázquez Cruz <rodrigo_velazquez99@ciencias.unam.mx>
2018-09-07 14:40:35 -0500

    Cambios a Usuario

--- Usuario/Usuario.go
+++ Usuario/Usuario.go
@@ -21,3 +21,12 @@ func AgregaUsuario(nuevoNombre string, nuevaConexion net.Conn) {
   nuevoUsuario := User{ nombre: nuevoNombre, conexion: nuevaConexion }
   listaUsuarios = append(listaUsuarios, nuevoUsuario)
 }
+
+func buscaUsuario(conexion net.Conn) string {
+  for _,usuario := range var {
+    if conexion == usuario.conexion{
+      return usuario.nombre
+    }
+  }
+  return ""
+}
