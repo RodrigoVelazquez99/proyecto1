c7195d2cdd58229191140bcf4961271899028138
Rodrigo Fernando Velázquez Cruz <rodrigo_velazquez99@ciencias.unam.mx>
2018-09-09 23:18:53 -0500

    Varios clientes se pueden conectar

--- Cliente/Cliente.go
+++ Cliente/Cliente.go
@@ -1,58 +1,75 @@
-package main
+	package main
 
-import (
-	"bufio"
-	"fmt"
-	"net"
-	"os"
-)
-
-func main() {
-
-	if len(os.Args) != 3 {
-		fmt.Println("No agregaste correctamente los datos de la direccion y puerto")
-		os.Exit(1)
-	}
-
+	import (
+		"bufio"
+		"fmt"
+		"net"
+		"os"
+		"strings"
+	)
 	var nombre string
 
-	fmt.Println(" Introduce el nombre de usuario ")
-	fmt.Scanln(& nombre)
-	fmt.Println("Bienvenido " + nombre)
-	var conexion net.Conn
-	var err error
-	for {
-		conexion, err = net.Dial("tcp", os.Args[1] + ":" + os.Args[2])
-		revisaError(err)
+	func main() {
+
+		if len(os.Args) != 3 {
+			fmt.Println("No agregaste correctamente los datos de la direccion y puerto")
+			os.Exit(1)
+		}
+		fmt.Println(" Introduce el nombre de usuario ")
+		fmt.Scanln(& nombre)
+		fmt.Println("Bienvenido " + nombre)
+		fmt.Println("				Comandos del chat" + "\n" +
+										"/close		Cerrar sesion	  	" + "\n" +
+										"/" + "\n" )
+		var conexion net.Conn
+		var err error
+		for {
+			conexion, err = net.Dial("tcp", os.Args[1] + ":" + os.Args[2])
+			if err == nil {
+				break
+			}
+		}
+		defer conexion.Close()
+		go recibeMensajes(conexion)
+		enviaMensajes(conexion)
+
 	}
 
-	defer conexion.Close()
-	go recibeMensajes(conexion)
-	enviaMensajes(conexion, nombre)
-
-}
-
-func enviaMensajes(conexion net.Conn, nombre string){
-	for {
-		fmt.Print(nombre + ">")
-		mensaje, _ := bufio.NewReader(conexion).ReadString('\n')
-		mensaje = nombre + ":" + mensaje
-		conexion.Write([]byte(mensaje))
+	func enviaMensajes(conexion net.Conn){
+		online := true
+		for {
+			for  online {
+				conexion.Write([]byte(nombre + " se ha conectado" + "\n"))
+				online = false
+			}
+			fmt.Print(nombre + ">")
+			lector := bufio.NewReader(os.Stdin)
+			mensaje, err := lector.ReadString('\n')
+			if strings.Contains(mensaje, "/close") {
+				fmt.Fprintf(conexion, nombre + " se ha desconectado " + "\n")
+				break
+			}
+			if err != nil {
+				break
+			}
+			mensaje = nombre + ":" + mensaje
+			fmt.Fprintf(conexion, mensaje)
+		}
 	}
-}
 
-func recibeMensajes(conexion net.Conn) {
-	for {
-		informacionDevuelta := bufio.NewReader(os.Stdin)
-		lector, _ := informacionDevuelta.ReadString('\n')
-		fmt.Fprintf(conexion, lector+"\n")
+	func recibeMensajes(conexion net.Conn) {
+		for {
+			informacionDevuelta, err := bufio.NewReader(conexion).ReadString('\n')
+			if err != nil {
+				break
+			}
+			fmt.Print("\n" + string(informacionDevuelta))
+		}
 	}
-}
 
-
-func revisaError(err error) {
-	if err != nil {
-		fmt.Println("Fallo la conexion al servidor")
-		os.Exit(1)
+	func revisaError(err error) {
+		if err != nil {
+			fmt.Println("Fallo la conexion al servidor")
+			os.Exit(1)
+		}
 	}
-}
--- Servidor/Servidor.go
+++ Servidor/Servidor.go
@@ -1,68 +1,75 @@
-package main
+	package main
 
-import (
-	"bufio"
-	"fmt"
-	"net"
-	"os"
-	"log"
-	"io"
-)
+	import (
+		"fmt"
+		"net"
+		"os"
+		"io"
+		"bytes"
+	)
 
-var listaConexiones []net.Conn
+	var listaConexiones []net.Conn
 
-func main() {
+	func main() {
 
-	if len(os.Args) != 3 {
-		fmt.Println("No agregaste correctamente los datos de la direccion y puerto")
-		os.Exit(1)
-	}
-
-	fmt.Println("  Iniciando el servidor .....  ")
-	servidor, err := net.Listen("tcp", os.Args[1] + ":" + os.Args[2])
- 	revisaError(err)
-	listaConexiones = make([]net.Conn,1)
-
-	for {
-		conexion, disponible := servidor.Accept()
-		listaConexiones = append(listaConexiones, conexion)
-		if disponible != nil {
-			fmt.Println("Conexion fallida")
+		if len(os.Args) != 3 {
+			fmt.Println("No agregaste correctamente los datos de la direccion y puerto")
 			os.Exit(1)
 		}
-		go manejaCliente(conexion)
-	}
-}
 
-func revisaError(err error) {
-	if err != nil {
-		fmt.Println("Fallo la conexion al servidor")
-		os.Exit(1)
-	}
-}
+		fmt.Println("  Iniciando el servidor .....  ")
+		servidor, err := net.Listen("tcp", os.Args[1] + ":" + os.Args[2])
+	 	revisaError(err)
+		listaConexiones = make([]net.Conn, 1)
 
-func manejaCliente(conexion net.Conn) {
-	defer conexion.Close()
-	mensaje := make([]byte,256)
-	for  {
-		for  {
-			cadena, err := conexion.Read(mensaje)
-			if err != nill{
-				if err == io.EOF {
-					fmt.Println("Ocurrio un error")
-					os.Exit(1)
-				}
-				os.Exit(1)
+		for {
+			conexion, disponible := servidor.Accept()
+			revisaError(disponible)
+			if conexion != nil {
+				listaConexiones = append(listaConexiones, conexion)
 			}
-			mensaje = bytes.Trim(mensaje[:cadena], "\x00")
+			go manejaCliente(conexion)
 		}
-		enviaClientes(mensaje, conexion)
-		mensaje = make([]byte, 0)
 	}
-}
 
-func enviaClientes(mensaje byte[], cliente net.Conn){
-	for _, conexion := range listaConexiones {
-		conexion.Write(mensaje)
+	func revisaError(err error) {
+		if err != nil {
+			fmt.Println("Fallo la conexion al servidor")
+			os.Exit(1)
+		}
 	}
-}
+
+	func manejaCliente(conexion net.Conn) {
+		defer conexion.Close()
+		mensaje := make([]byte,256)
+		var tmp []byte
+		for {
+			for {
+				cadena, err := conexion.Read(mensaje)
+				if err != nil{
+					if err == io.EOF {
+						break
+					}
+				}
+				mensaje = bytes.Trim(mensaje[:cadena], "\x00")
+				tmp = append(tmp, mensaje...)
+				if tmp[len(tmp) - 1 ] == 10 {
+					break
+				}
+			}
+			enviaClientes(tmp, conexion)
+			tmp = make([]byte, 0)
+		}
+	}
+
+	func enviaClientes(mensaje []byte, conexionActual net.Conn){
+		for _, conexion := range listaConexiones {
+			if conexion != nil{
+				if conexion == conexionActual{
+					continue
+				} else {
+					conexion.Write(mensaje)
+				}
+			}
+		}
+	}
